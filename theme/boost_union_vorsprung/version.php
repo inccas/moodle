ec6ea59d4f691c6fe38e9d6cfdba001df6f79e62
DanouNauck <Danou@nauck.eu>
2025-02-05 11:53:35 +0100

    more security features

--- theme/boost_union_vorsprung/version.php
+++ theme/boost_union_vorsprung/version.php
@@ -25,6 +25,6 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'theme_boost_union_vorsprung';
-$plugin->version = 2024010104;
+$plugin->version = 2025010100;
 $plugin->maturity = MATURITY_STABLE;
 $plugin->dependencies = ['theme_boost_union' => 2024100700];
