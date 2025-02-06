52dfeffa7c79676bf92d6fa2f036bc9529cd4569
DanouNauck <Danou@nauck.eu>
2025-02-06 13:55:08 +0100

    adding lots of new plugins and other features to the base of moodle

---
+++ mod/assign/submission/geogebra/lang/he/assignsubmission_geogebra.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * Strings for component 'assignsubmission_geogebra'
+ *
+ * @package        assignsubmission_geogebra
+ * @author         Christoph Stadlbauer <christoph.stadlbauer@geogebra.org>
+ * @copyright  (c) International GeoGebra Institute 2014
+ * @license        http://www.geogebra.org/license
+ */
+ 
+$string['geogebra'] = 'GeoGebra';
+$string['enabled'] = 'GeoGebra';
+$string['algebra'] = 'אלגברה';
+$string['allowgeogebrasubmissions'] = 'מופעל';
+$string['cas'] = 'חישוב אלגברי';
+$string['default'] = 'מופעל כברירת מחדל';
+$string['default_help'] = 'אם מוגדרת שיטת ההגשה תהיה מופעלת כברירת מחדל לכל המשימות החדשות.';
+$string['dragndrop'] = 'Drag and drop a GeoGebra file anywhere on the GeoGebra Applet section';
+$string['enabled_help'] = 'אם מופעל, תלמידים יכולים להשתמש בגאוגברה להגשות שלהם.';
+$string['eventassessableuploaded'] = 'הגשה בגאוגברה הועלתה:';
+$string['geogebrasubmission'] = 'הרשה הגשות גאוגברה';
+$string['geometry'] = 'גאומטריה';
+$string['ggbtemplates'] = 'בחר תבנית';
+$string['ggbturl'] = 'תבנית גאוגברה לשימוש התלמיד';
+$string['ggbturl_help'] = 'ההיבט או חומר GeoGebra המשמש כתבנית לתלמיד.';
+$string['perspective3d'] = 'גרפיקה תלת ממדית';
+$string['pluginname'] = 'הגשות גאוגברה';
+$string['probCalc'] = 'מחשבון הסתברות';
+$string['spreadsheet'] = 'גליון';
+$string['useafile'] = '... or use a ggb-file';
+$string['useafile_help'] = 'You may also use a ggb-file. Make sure that you enable GeoGebra submission type first!';
+$string['userdefined'] = 'משתמשת לא מוגדרת:';
