f409e65236b9eb4d30f707a385bd9f2a3d76dd07
DanouNauck <Danou@nauck.eu>
2025-02-24 14:45:54 +0100

    removing old version of stash

--- blocks/stash/amd/src/local/leaderboard/most_singular_item.js
+++
@@ -1,8 +0,0 @@
-export const init = (id, itemid) => {
-    const formelement = document.querySelector('input[name="leaderboard-options-' + id + '"]');
-    formelement.dataset.options = itemid;
-    const options = document.querySelector('select[name="msi_options"]');
-    options.addEventListener('change', (e) => {
-        formelement.dataset.options = e.currentTarget.value;
-    });
-};
