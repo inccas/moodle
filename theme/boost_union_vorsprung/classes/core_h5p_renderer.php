9a1cdba5126b86351949bbfbba364152d0ece0d8
DanouNauck <Danou@nauck.eu>
2025-02-27 13:12:11 +0100

    many new changes im theme

--- theme/boost_union_vorsprung/classes/core_h5p_renderer.php
+++ theme/boost_union_vorsprung/classes/core_h5p_renderer.php
@@ -1,6 +1,6 @@
 <?php
 
-use theme_vorsprung\helper\h5p_helper;
+use theme_boost_union_vorsprung\helper\h5p_helper;
 
 /**
  * Vorsprung theme.
@@ -15,7 +15,7 @@ global $CFG;
 if (file_exists($CFG->dirroot.'/h5p/classes/output/renderer.php')) {
     if (method_exists(\core_h5p\output\renderer::class, 'h5p_alter_styles')) {
         /**
-         * Class theme_vorsprung_core_h5p_renderer.
+         * Class theme_boost_union_vorsprung_core_h5p_renderer.
          *
          * See: https://tracker.moodle.org/browse/MDL-69087 and
          *      https://github.com/sarjona/h5pmods-moodle-plugin.
@@ -23,7 +23,7 @@ if (file_exists($CFG->dirroot.'/h5p/classes/output/renderer.php')) {
          * @package     theme_vorsprung
          * @copyright   2023 Jörg Hagemann
          */
-        class theme_vorsprung_core_h5p_renderer extends \core_h5p\output\renderer {
+        class theme_boost_union_vorsprung_core_h5p_renderer extends \core_h5p\output\renderer {
             use h5p_helper;
 
             public function h5p_alter_styles(&$styles, $libraries, $embedtype) {
@@ -39,12 +39,12 @@ if (file_exists($h5prenderer)) {
     require_once($h5prenderer);
 
     /**
-     * Class theme_vorsprung_mod_hvp_renderer
+     * Class theme_boost_union_vorsprung_mod_hvp_renderer
      *
      * @package     theme_vorsprung
      * @copyright   2023 Jörg Hagemann
      */
-    class theme_vorsprung_mod_hvp_renderer extends mod_hvp_renderer {
+    class theme_boost_union_vorsprung_mod_hvp_renderer extends mod_hvp_renderer {
         use h5p_helper;
 
         public function hvp_alter_styles(&$styles, $libraries, $embedtype) {
---
+++ theme/boost_union_vorsprung/classes/helper/h5p_helper.php
@@ -0,0 +1,48 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Vorsprung theme.
+ *
+ * @package     theme_vorsprung
+ * @copyright   2023 Jörg Hagemann
+ */
+
+/**
+ * Trait theme_vorsprung_core_h5p_renderer.
+ *
+ * See: https://tracker.moodle.org/browse/MDL-69087 and
+ *      https://github.com/sarjona/h5pmods-moodle-plugin.
+ *
+ * @package     theme_vorsprung
+ * @copyright   2023 Jörg Hagemann
+ */
+trait h5p_helper {
+    /**
+     * Add styles when an H5P is displayed.
+     *
+     * @param array $styles Styles that will be applied.
+     * @param array $libraries Libraries that will be shown.
+     * @param string $embedtype How the H5P is displayed.
+     */
+    protected function hvp_helper_alter_styles(&$styles, $libraries, $embedtype) {
+        global $CFG;
+        $styles[] = (object)array(
+            'path' => $CFG->httpswwwroot . '/theme/boost_union_vorsprung/scss/h5p.css',
+            'version' => '?ver=0.0.1',
+        );
+    }
+}
